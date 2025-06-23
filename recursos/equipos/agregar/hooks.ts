e55476bdf26bed98020bc84ac4d7fe1f5f4eb3f3
Francisco <frandelosantoss12@gmail.com>
2025-06-22 21:40:23 -0300

    Validaciones

--- recursos/equipos/agregar/hooks.ts
+++ recursos/equipos/agregar/hooks.ts
@@ -1,278 +1,282 @@
-
 //-- Se importan dependencias, como hooks y componentes--
-import { useState, useEffect } from 'react'
-import { Cliente, Modelo, Marca, TipoDeEquipo } from './types'
+import { useEffect, useState } from 'react'
+import { Cliente, Marca, Modelo, TipoDeEquipo } from './types'
 
 export function useAgregarEquipo() {
+	// Se declaran variables
 
+	const [successMessage, setSuccessMessage] = useState('')
+	const [errorMessage, setErrorMessage] = useState('')
 
-    // Se declaran variables
+	///Variables que utilizo para: el input a la hora de agregar un nuevo tipo de equipo, para abrir y cerrar el modal, y para el array que utilizo a la hora de mostrar los equipos
+	const [nuevoTipo, setNuevoTipo] = useState('')
+	const [modalTDEAbierto, setModalTDEAbierto] = useState(false)
+	const [tipoDeEquipo, setTipoDeEQuipo] = useState<TipoDeEquipo[]>([])
 
-    const [successMessage, setSuccessMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+	///Variables que utilizo para: el input a la hora de agregar una marca, para abrir y cerrar el modal, y para el array que utilizo a la hora de mostrar las marcas
+	const [nuevaMarca, setNuevaMarca] = useState('')
+	const [modalMarcaAbierto, setModalMarcaAbierto] = useState(false)
+	const [marcas, setMarcas] = useState<Marca[]>([])
 
+	///Variables que utilizo para: el input a la hora de agregar un modelo, para abrir y cerrar el modal, y para el array que utilizo a la hora de mostrar los modelos
+	const [nuevoModelo, setNuevoModelo] = useState('')
+	const [modalModeloAbierto, setModalModeloAbierto] = useState(false)
+	const [modelos, setModelos] = useState<Modelo[]>([])
 
-    ///Variables que utilizo para: el input a la hora de agregar un nuevo tipo de equipo, para abrir y cerrar el modal, y para el array que utilizo a la hora de mostrar los equipos
-    const [nuevoTipo, setNuevoTipo] = useState('')
-    const [modalTDEAbierto, setModalTDEAbierto] = useState(false);
-    const [tipoDeEquipo, setTipoDeEQuipo] = useState<TipoDeEquipo[]>([])
+	///Variables que utilizo para traer el array de clientes y luego poder mostrarlos
+	const [clientes, setClientes] = useState<Cliente[]>([])
 
-    ///Variables que utilizo para: el input a la hora de agregar una marca, para abrir y cerrar el modal, y para el array que utilizo a la hora de mostrar las marcas
-    const [nuevaMarca, setNuevaMarca] = useState('')
-    const [modalMarcaAbierto, setModalMarcaAbierto] = useState(false);
-    const [marcas, setMarcas] = useState<Marca[]>([])
+	/// Objeto que contiene los datos actuales del formulario para luego agregar un equipo
+	const [formData, setFormData] = useState({
+		idCliente: '',
+		nroSerie: '',
+		idMarca: '',
+		idModelo: '',
+		razonDeIngreso: '',
+		observaciones: '',
+		enciende: false,
+		idTipoDeEquipo: '',
+	})
 
-    ///Variables que utilizo para: el input a la hora de agregar un modelo, para abrir y cerrar el modal, y para el array que utilizo a la hora de mostrar los modelos
-    const [nuevoModelo, setNuevoModelo] = useState('')
-    const [modalModeloAbierto, setModalModeloAbierto] = useState(false);
-    const [modelos, setModelos] = useState<Modelo[]>([])
+	/// Para inputs tipo texto o select
+	const handleChange = (
+		e: React.ChangeEvent<
+			HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement
+		>
+	) => {
+		const { name, value } = e.target
+		setFormData((prev) => ({ ...prev, [name]: value }))
+	}
 
-    ///Variables que utilizo para traer el array de clientes y luego poder mostrarlos
-    const [clientes, setClientes] = useState<Cliente[]>([])
+	/// Para checkbox (enciende)
+	const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, checked } = e.target
+		setFormData((prev) => ({ ...prev, [name]: checked }))
+	}
 
-    /// Objeto que contiene los datos actuales del formulario para luego agregar un equipo
-    const [formData, setFormData] = useState({
-        idCliente: '',
-        nroSerie: '',
-        idMarca: '',
-        idModelo: '',
-        razonDeIngreso: '',
-        observaciones: '',
-        enciende: false,
-        idTipoDeEquipo: '',
-    })
+	//Funcion asincronica que se e ejecuta para agregar un equipo
+	const handleSubmit = async () => {
+		setErrorMessage('')
+		setSuccessMessage('')
+		// Validaciones básicas
 
-    /// Para inputs tipo texto o select
-    const handleChange = (
-        e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
-    ) => {
-        const { name, value } = e.target;
-        setFormData(prev => ({ ...prev, [name]: value }));
-    };
+		if (
+			!formData.idCliente ||
+			!formData.idTipoDeEquipo ||
+			!(formData.nroSerie || '').trim() ||
+			!formData.idMarca ||
+			!formData.idModelo ||
+			!(formData.razonDeIngreso || '').trim() ||
+			!(formData.observaciones || '').trim()
+		) {
+			setErrorMessage('Por favor complete todos los campos obligatorios.')
+			return
+		}
 
+		try {
+			const nuevoEquipo = {
+				idCliente: Number(formData.idCliente),
+				idTipoDeEquipo: Number(formData.idTipoDeEquipo),
+				nroSerie: formData.nroSerie,
+				idMarca: Number(formData.idMarca),
+				idModelo: Number(formData.idModelo),
+				razonDeIngreso: formData.razonDeIngreso,
+				observaciones: formData.observaciones,
+				enciende: formData.enciende,
+			}
 
-    /// Para checkbox (enciende)
-    const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { name, checked } = e.target
-        setFormData(prev => ({ ...prev, [name]: checked }))
-    }
+			const response = await fetch('/api/equipos', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(nuevoEquipo),
+			})
 
-    //Funcion asincronica que se e ejecuta para agregar un equipo
-    const handleSubmit = async () => {
-        setErrorMessage('');
-        setSuccessMessage('');
-        // Validaciones básicas
+			if (!response.ok) throw new Error('Error al agregar equipo')
 
-        if (
-            !formData.idCliente ||
-            !formData.idTipoDeEquipo ||
-            !(formData.nroSerie || '').trim() ||
-            !formData.idMarca ||
-            !formData.idModelo ||
-            !(formData.razonDeIngreso || '').trim() ||
-            !(formData.observaciones || '').trim()
-        ) {
-             setErrorMessage('Por favor complete todos los campos obligatorios.');
-            return;
-        }
+			const data = await response.json()
+			console.log('Equipo agregado:', data)
 
-        try {
-            const nuevoEquipo = {
-                idCliente: Number(formData.idCliente),
-                idTipoDeEquipo: Number(formData.idTipoDeEquipo),
-                nroSerie: formData.nroSerie,
-                idMarca: Number(formData.idMarca),
-                idModelo: Number(formData.idModelo),
-                razonDeIngreso: formData.razonDeIngreso,
-                observaciones: formData.observaciones,
-                enciende: formData.enciende,
-            };
+			setSuccessMessage('Equipo agregado exitosamente.')
 
-            const response = await fetch('/api/equipos', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(nuevoEquipo),
-            });
+			// Limpiar formulario
+			setFormData({
+				idCliente: '',
+				idTipoDeEquipo: '',
+				nroSerie: '',
+				idMarca: '',
+				idModelo: '',
+				razonDeIngreso: '',
+				observaciones: '',
+				enciende: false,
+			})
+		} catch (error) {
+			console.error(error)
+			setErrorMessage('Error al agregar equipo.')
+		}
+	}
 
-            if (!response.ok) throw new Error('Error al agregar equipo');
+	// Funcion asincronica que agrega un tipoDeEquipo nuevo
+	async function agregarTipoDeEquipo() {
+		if (!nuevoTipo.trim()) return
+		try {
+			const res = await fetch('/api/tipoDeEquipo', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ descripcion: nuevoTipo }),
+			})
 
-            const data = await response.json();
-            console.log('Equipo agregado:', data);
+			if (res.ok) {
+				const nuevo = await res.json()
+				///Actualiza la lista
+				setTipoDeEQuipo((prev) => [...prev, nuevo])
+				///Borra el input
+				setNuevoTipo('')
+				///Cierra el modal
+				setModalTDEAbierto(false)
+			} else {
+				const error = await res.text()
+				console.error('Error en la API:', error)
+				alert('Error al agregar el tipo de equipo: ' + error)
+			}
+		} catch (error) {
+			console.error('Error al hacer fetch:', error)
+		}
+	}
 
-            setSuccessMessage('Equipo agregado exitosamente.');
+	// Funcion asincronica que agrega un modelo nuevo
+	async function agregarModelo() {
+		if (!nuevoModelo.trim()) return
+		try {
+			const res = await fetch('/api/modelos', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ descripcion: nuevoModelo }),
+			})
 
-            // Limpiar formulario
-            setFormData({
-                idCliente: '',
-                idTipoDeEquipo: '',
-                nroSerie: '',
-                idMarca: '',
-                idModelo: '',
-                razonDeIngreso: '',
-                observaciones: '',
-                enciende: false,
-            });
-        } catch (error) {
-            console.error(error);
-            setErrorMessage('Error al agregar equipo.');
-        }
-    };
+			if (res.ok) {
+				const nuevo = await res.json()
+				///Actualiza la lista
+				setModelos((prev) => [...prev, nuevo])
+				///Borra el input
+				setNuevoModelo('')
+				///Cierra el modal
+				setModalModeloAbierto(false)
+			} else {
+				const error = await res.text()
+				console.error('Error en la API:', error)
+				alert('Error al agregar el modelo: ' + error)
+			}
+		} catch (error) {
+			console.error('Error al hacer fetch:', error)
+		}
+	}
 
-    // Funcion asincronica que agrega un tipoDeEquipo nuevo
-    async function agregarTipoDeEquipo() {
-        if (!nuevoTipo.trim()) return;
-        try {
+	// Funcion asincronica que agrega una marca nueva
+	async function agregarMarca() {
+		if (!nuevaMarca.trim()) return
+		try {
+			const res = await fetch('/api/marcas', {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify({ descripcion: nuevaMarca }),
+			})
 
-            const res = await fetch('/api/tipoDeEquipo', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ descripcion: nuevoTipo })
-            });
+			if (res.ok) {
+				const nuevo = await res.json()
+				///Actualiza la lista
+				setMarcas((prev) => [...prev, nuevo])
+				///Borra el input
+				setNuevaMarca('')
+				///Cierra el modal
+				setModalMarcaAbierto(false)
+			} else {
+				const error = await res.text()
+				console.error('Error en la API:', error)
+				alert('Error al agregar el la marca: ' + error)
+			}
+		} catch (error) {
+			console.error('Error al hacer fetch:', error)
+		}
+	}
 
-            if (res.ok) {
-                const nuevo = await res.json()
-                ///Actualiza la lista
-                setTipoDeEQuipo((prev) => [...prev, nuevo]);
-                ///Borra el input
-                setNuevoTipo('')
-                ///Cierra el modal
-                setModalTDEAbierto(false)
-            }
-            else {
-                const error = await res.text();
-                console.error('Error en la API:', error);
-                alert('Error al agregar el tipo de equipo: ' + error);
-            }
-        }
-        catch (error) {
-            console.error('Error al hacer fetch:', error);
-        }
-    }
+	useEffect(() => {
+		//Funciones que utilizo para traer los clientes, modelos, marcas y tipos de equipos (respectivamente) existentes de mi base de datos y poder mostrarlos
 
-    // Funcion asincronica que agrega un modelo nuevo
-    async function agregarModelo() {
-        if (!nuevoModelo.trim()) return;
-        try {
+		async function cargarClientes() {
+			try {
+				const res = await fetch('/api/clientes')
+				const data = await res.json()
+				const clientesActivos = data.filter(
+					(cliente: any) => cliente.deleted === false
+				)
+				setClientes(clientesActivos)
+			} catch (error) {
+				console.error('Error al cargar clientes', error)
+			}
+		}
 
-            const res = await fetch('/api/modelos', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ descripcion: nuevoModelo })
-            });
+		async function cargarModelos() {
+			try {
+				const res = await fetch('/api/modelos')
+				const data = await res.json()
+				setModelos(data)
+			} catch (error) {
+				console.error('Error al cargar el modelo', error)
+			}
+		}
 
-            if (res.ok) {
-                const nuevo = await res.json()
-                ///Actualiza la lista
-                setModelos((prev) => [...prev, nuevo]);
-                ///Borra el input
-                setNuevoModelo('')
-                ///Cierra el modal
-                setModalModeloAbierto(false)
-            }
-            else {
-                const error = await res.text();
-                console.error('Error en la API:', error);
-                alert('Error al agregar el modelo: ' + error);
-            }
-        }
-        catch (error) {
-            console.error('Error al hacer fetch:', error);
-        }
-    }
+		async function cargarMarcas() {
+			try {
+				const res = await fetch('/api/marcas')
+				const data = await res.json()
+				setMarcas(data)
+			} catch (error) {
+				console.error('Error al cargar la marca', error)
+			}
+		}
 
-    // Funcion asincronica que agrega una marca nueva
-    async function agregarMarca() {
-        if (!nuevaMarca.trim()) return;
-        try {
+		async function cargarTipoDeEquipo() {
+			try {
+				const res = await fetch('/api/tipoDeEquipo')
+				const data = await res.json()
+				setTipoDeEQuipo(data)
+			} catch (error) {
+				console.error('Error al cargar el tipo de equipo', error)
+			}
+		}
 
-            const res = await fetch('/api/marcas', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ descripcion: nuevaMarca })
-            });
+		cargarTipoDeEquipo()
+		cargarMarcas()
+		cargarClientes()
+		cargarModelos()
+	}, [])
 
-            if (res.ok) {
-                const nuevo = await res.json()
-                ///Actualiza la lista
-                setMarcas((prev) => [...prev, nuevo]);
-                ///Borra el input
-                setNuevaMarca('')
-                ///Cierra el modal
-                setModalMarcaAbierto(false)
-            }
-            else {
-                const error = await res.text();
-                console.error('Error en la API:', error);
-                alert('Error al agregar el la marca: ' + error);
-            }
-        }
-        catch (error) {
-            console.error('Error al hacer fetch:', error);
-        }
-    }
-
-    useEffect(() => {
-        //Funciones que utilizo para traer los clientes, modelos, marcas y tipos de equipos (respectivamente) existentes de mi base de datos y poder mostrarlos 
-
-        async function cargarClientes() {
-            try {
-                const res = await fetch('/api/clientes')
-                const data = await res.json()
-                const clientesActivos = data.filter((cliente: any) => cliente.deleted === false)
-                setClientes(clientesActivos)
-            } catch (error) {
-                console.error('Error al cargar clientes', error)
-            }
-        }
-
-        async function cargarModelos() {
-            try {
-                const res = await fetch('/api/modelos')
-                const data = await res.json()
-                setModelos(data)
-            }
-            catch (error) {
-                console.error('Error al cargar el modelo', error)
-            }
-
-        }
-
-        async function cargarMarcas() {
-            try {
-                const res = await fetch('/api/marcas')
-                const data = await res.json()
-                setMarcas(data)
-            }
-            catch (error) {
-                console.error('Error al cargar la marca', error)
-            }
-        }
-
-        async function cargarTipoDeEquipo() {
-            try {
-                const res = await fetch('/api/tipoDeEquipo')
-                const data = await res.json()
-                setTipoDeEQuipo(data)
-            }
-            catch (error) {
-                console.error('Error al cargar el tipo de equipo', error)
-            }
-        }
-
-        cargarTipoDeEquipo()
-        cargarMarcas()
-        cargarClientes()
-        cargarModelos()
-    }, [])
-
-
-    return {
-        //Retorno todas las variables y metodos para poder llamarlos desde mi vista
-        nuevoTipo, setNuevoTipo, modalTDEAbierto, setModalTDEAbierto, tipoDeEquipo,
-        nuevaMarca, setNuevaMarca, modalMarcaAbierto, setModalMarcaAbierto, marcas,
-        nuevoModelo, setNuevoModelo, modalModeloAbierto, setModalModeloAbierto, modelos,
-        clientes, formData, handleChange, handleCheckboxChange, handleSubmit,
-        agregarTipoDeEquipo, agregarMarca, agregarModelo, successMessage, errorMessage
-    }
-
+	return {
+		//Retorno todas las variables y metodos para poder llamarlos desde mi vista
+		nuevoTipo,
+		setNuevoTipo,
+		modalTDEAbierto,
+		setModalTDEAbierto,
+		tipoDeEquipo,
+		nuevaMarca,
+		setNuevaMarca,
+		modalMarcaAbierto,
+		setModalMarcaAbierto,
+		marcas,
+		nuevoModelo,
+		setNuevoModelo,
+		modalModeloAbierto,
+		setModalModeloAbierto,
+		modelos,
+		clientes,
+		formData,
+		handleChange,
+		handleCheckboxChange,
+		handleSubmit,
+		agregarTipoDeEquipo,
+		agregarMarca,
+		agregarModelo,
+		successMessage,
+		errorMessage,
+	}
 }
